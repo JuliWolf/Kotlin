7712d47b76e2a2ee70034e056d2fe462f44b5d1b
Julia Ivanova <for.julia96@gmail.com>
2022-04-07 21:17:21 +0300

    String templates

---
+++ 3. Fundamentals/src/2_String-templates/Main.kt
@@ -0,0 +1,10 @@
+package `2_String-templates`
+
+fun main() {
+  // Strings
+  val myStr = "Hello World"
+  var firCharInStr = myStr[0]
+  var lastCharInStr = myStr[myStr.length - 1]
+
+  print("First character $firCharInStr and the length of myStr is ${myStr.length}")
+}
