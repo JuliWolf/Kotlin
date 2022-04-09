1e3e8d0e33fd24c3340ea0051b43ae9b09e247a4
Julia Ivanova <for.julia96@gmail.com>
2022-04-09 15:35:43 +0300

    Create classes and objects

---
+++ 4. Object-oriented/src/2_Classes-and-objects/Main.kt
@@ -0,0 +1,21 @@
+package `2_Classes-and-objects`
+
+fun main() {
+  val julia: Person = Person("Julia", "Wolf")
+  val john: Person = Person()
+  val johnPeterson: Person = Person(lastName = "Peterson")
+}
+
+//class Person constructor (firstName: String, lastName: String) {
+//
+//}
+
+// Alternative
+// use `=` to set default values
+class Person (firstName: String = "John", lastName: String = "Doe") {
+
+//  Initializer Block
+  init {
+    println("Initialized a new Person object with firstName = $firstName and lastName = $lastName")
+  }
+}
